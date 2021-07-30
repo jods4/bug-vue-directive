d06aa3e7c58a61d7e02dfbff85e93acf7dd137a0
Joel Dumas <joel.dumas@elca.ch>
2021-07-30 15:13:01 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" v-dir />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import VDir from './dir'
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
